140c1ffc76fdba40318c4e9337cf12aecc5b0a36
Vien Quoc Anh <quocanhvien99@gmail.com>
2021-12-30 23:06:32 +0700

    text to speech feature

--- bot.ts
+++ bot.ts
@@ -1,30 +1,78 @@
-import { Client, Intents, MessageAttachment, TextChannel } from 'discord.js';
+import {
+	Client,
+	MessageAttachment,
+	TextChannel,
+	VoiceChannel,
+} from 'discord.js';
+import {
+	AudioPlayer,
+	createAudioPlayer,
+	createAudioResource,
+	joinVoiceChannel,
+} from '@discordjs/voice';
+import textToSpeech from '@google-cloud/text-to-speech';
+import { Readable } from 'stream';
 require('dotenv').config();
 
 const client = new Client({
-	intents: ['GUILDS', 'GUILD_MESSAGES', 'DIRECT_MESSAGES'],
+	intents: [
+		'GUILDS',
+		'GUILD_MESSAGES',
+		'DIRECT_MESSAGES',
+		'GUILD_VOICE_STATES',
+	],
 });
+const ggclient = new textToSpeech.TextToSpeechClient();
+
+let listRoom = new Map<string, AudioPlayer>();
 
 client.on('ready', () => {
 	console.log(`Logged in`);
-	let chanel: any = client.channels.cache.get(process.env.CHANNEL_ID!);
-	const img = new MessageAttachment('./3.PNG', '3.png');
-	(chanel as TextChannel).send({ files: [img] });
+	//let chanel: any = client.channels.cache.get(process.env.CHANNEL_ID!);
+	// const img = new MessageAttachment('./test.PNG');
+	// const img1 = new MessageAttachment('./3.PNG');
+	// (chanel as TextChannel).send({ files: [img, img1] }).then((res) => {
+	// 	const atch = res.attachments;
+	// 	const arr = atch.map((x) => x.url);
+	// 	console.log(arr);
+	// });
 });
 
-client.on('interactionCreate', async (interaction) => {
-	console.log(interaction.isCommand());
-	if (!interaction.isCommand()) return;
-	if (interaction.commandName === 'ping') {
-		await interaction.reply('Pong!');
+client.on('messageCreate', async (msg) => {
+	if (msg.content.startsWith('!say')) {
+		const channel = msg.member!.voice.channel!;
+		const connection = joinVoiceChannel({
+			channelId: channel.id,
+			guildId: msg.guild!.id,
+			// @ts-ignore
+			adapterCreator: msg.guild.voiceAdapterCreator,
+		});
+		const player = createAudioPlayer();
+		listRoom.set(channel.id, player);
+		connection.subscribe(player);
+		const resc = createAudioResource(
+			Readable.from(await speech(msg.content.slice(5))),
+			{ inlineVolume: true }
+		);
+		player.play(resc);
 	}
 });
 
-client.on('messageCreate', (msg) => {
-	console.log(msg.content);
-	if (msg.content == 'ping') {
-		msg.channel.send('pong cái đm cáu vl');
-	}
-});
+async function speech(text: string) {
+	return new Promise<string>(async (resolve, reject) => {
+		const [response] = await ggclient.synthesizeSpeech({
+			input: { text: text },
+			// Select the language and SSML voice gender (optional)
+			voice: {
+				languageCode: 'vi-VN',
+				ssmlGender: 'FEMALE',
+				name: 'vi-VN-Wavenet-A',
+			},
+			// select the type of audio encoding
+			audioConfig: { audioEncoding: 'MP3' },
+		});
+		resolve(response.audioContent as string);
+	});
+}
 
 client.login(process.env.TOKEN);
